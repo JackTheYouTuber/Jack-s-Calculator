332a2b91c6b4e39acd07130a83ffd9ab88635b1c
JackTheYouTuber <naveedreza605@gmail.com>
2022-04-11 15:55:39 +0600

    added .venv and created main gui

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from tkinter import *
 
+
 root = Tk()
 
+# Lable declaration
+mainLable = Label(root, text="Jack's Calculator", padx=10, pady=5)
 
 
+#Button declaration
+simpleMathButton = Button(root, text="Simple Math", padx=10 , pady=5)
+complexMathButton = Button(root, text="Complex Math", padx=10 , pady=5)
+graphMathButton = Button(root, text="Graph Math", padx=10 , pady=5)
+
+#Button initalization
+simpleMathButton.grid(row=1,column=0)
+complexMathButton.grid(row=2,column=0)
+graphMathButton.grid(row=3,column=0)
+
+# Lable initalization
+mainLable.grid(row=0,column=0)
 
 root.mainloop()
